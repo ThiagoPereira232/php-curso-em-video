e40ac2ca922b2239f1e0dfaa676e555f5acdb04a
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2021-01-25 18:53:40 -0300

    1

---
+++ aula04.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aula 4</title>
+</head>
+<body>
+    <?php
+        $n = 4;
+        $no = "Thiago";
+        echo no;
+    ?>
+</body>
+</html>
