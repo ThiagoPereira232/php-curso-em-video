b2be8abe57d689bd7bdd4475524752b270ea5a08
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2021-01-25 18:53:32 -0300

    Create ola.php

---
+++ ola.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ola mundo</title>
+    <style>
+        h2 {
+            color: blue;
+            text-shadow: 1px 1px 1px black;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Testando o PHP</h1>
+    <?php
+        echo "<h2>Olá, <br> mundo!</h2>";
+    ?>
+</body>
+
+</html>
