569b576874dd5f16c3f57ed4e62e5505d9d8f35c
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2021-01-26 15:12:40 -0300

    Update aula04.php

--- aula04.php
+++ aula04.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" href="modelo/_css/estilo.css"/>
+  <link rel="stylesheet" href="modelo/css/estilo.css"/>
   <meta charset="UTF-8"/>
   <title>Curso de PHP - CursoemVideo.com</title>
 </head>
 <body>
 <div>
     <?php
-        
+        $n = 4;
+        // $no = (string)"Thiago";
+        $nome = "Thiago";
+        $idade = 15;
+        // echo $nome." tem ".$idade." anos.";
+        echo "$nome tem $idade anos.";
     ?>
 </div>
 </body>
