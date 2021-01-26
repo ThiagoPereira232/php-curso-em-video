672e16225b96e789e2b128258efcb3cf9643473a
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2021-01-26 15:29:36 -0300

    somador

---
+++ aula05/somador.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="modelo/css/estilo.css" />
+    <meta charset="UTF-8" />
+    <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+    <div>
+        <?php
+
+            $n1 = 3;
+            $n2 = 2;
+            $s = $n1 + $n2;
+
+            // echo "A soma entre $n1 e $n2 é igual a $s";
+            echo "A soma vale ".($n1 + $n2);
+
+        ?>
+    </div>
+</body>
+
+</html>
